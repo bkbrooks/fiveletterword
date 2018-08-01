70553463f3b3cefdcc8c84cce68badd3eea995cd
Blair Brooks <blairkbrooks@gmail.com>
2018-08-01 15:05:43 -0700

    added rack-cors to bypass cors issues with api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  def handle_options_request
+    headers['Access-Control-Allow-Headers'] =
+      'accept, authorization, content-type, X-Recoder-API-Version'
+    return head(:ok) if request.request_method == 'OPTIONS'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :games, except: [:delete, :update]
 
   post 'games/:game_id/make_guess', to: 'guesses#make_guess'
